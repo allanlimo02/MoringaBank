ef1b9b214b97b93e9a33dac9025518202390ea6a
allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-29 15:50:19 +0300

    Added form validation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+fu
+
+
 $(document).ready(function(){
     
-    let userName=document.getElementById('name').value;
+    let userName=document.getElementById('name');
     let inBal=document.getElementById('inBal').value;
     let withraw=document.getElementById('withraw').value;
     let deposit=document.getElementById('deposit').value;
 
+
+   $('.depositbtn').click(function(event){
+        $('#nameOutPut').text(userName.val())
+
+        event.preventDefault()
+   })
     
+
+
+
 })
