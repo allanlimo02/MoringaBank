53520107ffce666f5767f09ddd0617374561f93b
allanlimo02 <allan.limo@student.moringaschool.com>
2021-07-29 15:00:57 +0300

    Added button  styling

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    let userName=document.getElementById('name').value;
+    let inBal=document.getElementById('inBal').value;
+    let withraw=document.getElementById('withraw').value;
+    let deposit=document.getElementById('deposit').value;
+
+    
+})
